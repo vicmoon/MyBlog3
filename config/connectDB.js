332c5b72a25bc5619ddb301f05a4846d59ca80ec
Victoria Munteanu <munteanuvictoria1@gmail.com>
2024-08-29 15:30:38 +0200

    update

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongoURL");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("DB connected");
   } catch (error) {
     console.log("DB connection failed", error.message);
--- server.js
+++ server.js
@@ -1,10 +1,100 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 require("./config/connectDB");
 
 //middlewares
 
 //routes
+// user Routes
+// POST/api/v1/users/register
+app.post("/api/v1/users/register", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Registered",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// POST/api/v1/users/login
+app.post("/api/v1/users/login", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User logged in",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET/api/v1/users/:id
+app.get("/api/v1/users/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User details",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET/api/v1/users/profile/:id
+app.get("/api/v1/users/profile/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User profile",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// PUT/api/v1/users/profile-photo/:id
+app.put("/api/v1/users/profile-photo/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User profile image",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// PUT/api/v1/users/cover-photo/:id
+app.put("/api/v1/users/cover-photo/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User cover photo",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// PUT/api/v1/users/update-password/:id
+app.put("/api/v1/users/update-password/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User cover photo",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//post Route
+
+//comment Route
 
 //error handle middlewares
 
