4ff1539f4cad402fe3fcc4d551802b74dd783dc5
Victoria Munteanu <munteanuvictoria1@gmail.com>
2024-08-29 15:59:41 +0200

    add routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require("./config/connectDB");
 //middlewares
 
 //routes
+
 // user Routes
 // POST/api/v1/users/register
 app.post("/api/v1/users/register", async (req, res) => {
@@ -68,6 +69,17 @@ app.put("/api/v1/users/profile-photo/:id", async (req, res) => {
   }
 });
 
+// PUT/api/v1/users/update/:id
+app.put("/api/v1/users/update/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User update",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
 // PUT/api/v1/users/cover-photo/:id
 app.put("/api/v1/users/cover-photo/:id", async (req, res) => {
   try {
@@ -85,16 +97,134 @@ app.put("/api/v1/users/update-password/:id", async (req, res) => {
   try {
     res.json({
       status: "Success",
-      user: "User cover photo",
+      user: "User update password",
     });
   } catch (error) {
     res.json(error);
   }
 });
 
-//post Route
+//PUT/api/v1/users/logout/:id
+app.put("/api/v1/users/logout/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "User logout",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// POST ROUTES
+
+//POST/api/v1/posts/
+app.put("/api/v1/posts", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Post created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET/api/v1/posts/ (all)
+app.get("/api/v1/posts", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Post created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET/api/v1/posts/:id
+app.get("/api/v1/posts/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Post created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//DELETE/api/v1/posts/:id
+app.delete("/api/v1/posts/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Post deleted",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//PUT/api/v1/posts/:id
+app.put("/api/v1/posts/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Post edited",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 //comment Route
+app.put("/api/v1/comments", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Comment created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET/api/v1/comments/:id
+app.get("/api/v1/comments/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Comment created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//DELETE/api/v1/comments/:id
+app.delete("/api/v1/comments/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Comment deleted",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//PUT/api/v1/comments/:id
+app.put("/api/v1/comments/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "Success",
+      user: "Comment edited",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 //error handle middlewares
 
