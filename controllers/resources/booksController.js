f16b577c98ee54e00bb534a3d2f8642cdde132e3
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-09-16 21:19:51 +0200

    update resources

--- controllers/resources/booksController.js
+++ controllers/resources/booksController.js
@@ -25,7 +25,7 @@ const addBookController = async (req, res, next) => {
 //GET/api/v1/resources
 const getBookController = async (req, res) => {
   try {
-    const books = await Book.find();
+    const books = await Book.find().sort({ createdAt: -1 });
     res.render("resources", { books });
     // res.json({
     //   status: "Success",
