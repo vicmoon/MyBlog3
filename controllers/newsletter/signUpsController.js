324be768217c2f599e464307cbc127e1856b73ba
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-09-18 11:19:00 +0200

    update

--- controllers/newsletter/signUpsController.js
+++ controllers/newsletter/signUpsController.js
@@ -1,29 +1,25 @@
 const SignUp = require("../../model/newsletter/Signup");
 const appError = require("../../utils/appError");
 
-// const addSignUpController = async (req, res, next) => {
-//   console.log("Request Body:", req.body); // Log the incoming request body
+const addSignUpController = async (req, res, next) => {
+  console.log("Request Body:", req.body); // Log the incoming request body
 
-//   const { name, email } = req.body || {}; // Fallback to empty object if req.body is undefined
+  const { name, email } = req.body || {}; // Fallback to empty object if req.body is undefined
 
-//   if (!name || !email) {
-//     console.log("Missing Name or Email:", { name, email });
-//     return res.render("signup", {
-//       error: "⚠️ Name and Email are required ⚠️",
-//     });
-//   }
+  if (!name || !email) {
+    console.log("Missing Name or Email:", { name, email });
+    return res.render("signup", {
+      error: "⚠️ Name and Email are required ⚠️",
+    });
+  }
 
-//   try {
-//     // const signup = await SignUp.create({ name, email });
-//     res.redirect("/signup_success");
-//   } catch (error) {
-//     console.error("Error Details:", error);
-//     return next(appError(`An error occurred: ${error.message}`, 400));
-//   }
-// };
-
-const addSignUpController = (req, res) => {
-  res.send("Server is running. Form submission temporarily disabled.");
+  try {
+    // const signup = await SignUp.create({ name, email });
+    res.redirect("/signup_success");
+  } catch (error) {
+    console.error("Error Details:", error);
+    return next(appError(`An error occurred: ${error.message}`, 400));
+  }
 };
 
 const getSignUpController = async (req, res) => {
