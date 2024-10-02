7c6caa18fda778acc23ca60dfa7e6181614e49f9
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-10-02 21:13:25 +0200

    update all posts style

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get("/health", (req, res) => {
 //render homepage
 app.get("/", async (req, res) => {
   try {
-    const posts = await Post.find().sort({ createdAt: -1 }).limit(5);
+    const posts = await Post.find().sort({ createdAt: -1 }).limit(6);
     res.render("home", { posts });
   } catch (error) {
     res.render("home", { error: error });
