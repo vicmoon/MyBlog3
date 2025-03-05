d4c806b2b229a5b7f9eb4b7ecc64f9def6854a75
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-03-05 16:02:08 +0100

    update

--- controllers/posts/postsController.js
+++ controllers/posts/postsController.js
@@ -10,11 +10,9 @@ const postPostController = async (req, res, next) => {
       // return next(appError("Missing details, all fields are required"));
       return res.render('posts/addPost', { error: 'All fields are required' });
     }
-    // // find the user
-    // const userID = req.session.userAuth;
-    // const userFound = await User.findById(userID);
-
-    // console.log(userFound);
+    // find the user
+    const userID = req.session.userAuth;
+    const userFound = await User.findById(userID);
 
     //create the post
 
@@ -23,13 +21,14 @@ const postPostController = async (req, res, next) => {
       description,
       category,
       image: req.file.path,
+      user: userFound._id,
     });
 
-    // // push the post in the array user's posts
-    // userFound.posts.push(postCreated._id);
+    // push the post in the array user's posts
+    userFound.posts.push(postCreated._id);
 
-    // //save the user because we made changes
-    // await userFound.save();
+    //save the user because we made changes
+    await userFound.save();
     console.log('Post created');
     res.redirect('/');
   } catch (error) {
